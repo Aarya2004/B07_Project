517e09897efa33f503ce08fff5b6e0ec1aea2c06
Aarya2004 <aarya.prakash.2004@gmail.com>
2023-07-23 13:23:08 -0400

    initial commit

---
+++ app/src/main/java/com/example/b07project/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.b07project;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ownerLogin(View view) {
+        Intent intent = new Intent(this, StoreOwnerLogin.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/b07project/StoreOwnerLogin.java
@@ -0,0 +1,20 @@
+package com.example.b07project;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class StoreOwnerLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_owner_login);
+    }
+}
