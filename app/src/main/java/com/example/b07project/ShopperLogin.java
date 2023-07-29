f2208016e860201710d8c3d143ea7d7481c591f7
sabasheykhhassani <90866427+sabasheykhhassani@users.noreply.github.com>
2023-07-28 21:26:41 -0400

    Registration for Shopper.

--- app/src/main/java/com/example/b07project/ShopperLogin.java
+++ app/src/main/java/com/example/b07project/ShopperLogin.java
@@ -1,2 +1,47 @@
-package com.example.b07project;public class ShopperLogin {
+package com.example.b07project;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class ShopperLogin extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopper_login);
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/b07project/ShopperRegister.java
+++ app/src/main/java/com/example/b07project/ShopperRegister.java
@@ -1,2 +1,103 @@
-package com.example.b07project;public class ShopperRegister {
+package com.example.b07project;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ShopperRegister extends AppCompatActivity {
+    TextInputEditText editTextEmail;
+    TextInputEditText editTextPassword;
+    Button buttonRegister;
+    FirebaseAuth mAuth;
+    ProgressBar progressBarShopper;
+    TextView textViewShopper;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopper_register);
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.shopperEmail);
+        editTextPassword = findViewById(R.id.shopperPassword);
+        buttonRegister = findViewById(R.id.button_registerShopper);
+        progressBarShopper = findViewById(R.id.progressBarShopper);
+        textViewShopper = findViewById(R.id.loginNow);
+        textViewShopper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //to open the login activity intent
+                Intent intent = new Intent(getApplicationContext(), ShopperLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBarShopper.setVisibility(View.VISIBLE);
+                String emailShopper;
+                String passwordShopper;
+
+                emailShopper = String.valueOf(editTextEmail.getText());
+                passwordShopper = String.valueOf(editTextPassword.getText());
+
+                //check empty email or password
+                if (TextUtils.isEmpty(emailShopper)){
+                    Toast.makeText(ShopperRegister.this, "Must enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(passwordShopper)){
+                    Toast.makeText(ShopperRegister.this, "Must enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //implement create user
+
+                mAuth.createUserWithEmailAndPassword(emailShopper, passwordShopper)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBarShopper.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ShopperRegister.this, "Account created",
+                                            Toast.LENGTH_SHORT).show();
+
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(ShopperRegister.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+
+
+    }
+
+
 }
