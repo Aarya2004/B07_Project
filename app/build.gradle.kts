9f894f1bc51c82fd1ae93223b3b5235964d2fde1
joshuayu30 <62484135+joshuayu30@users.noreply.github.com>
2023-08-07 22:48:07 -0400

    Small visual update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.b07projectself"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.b07projectself"
