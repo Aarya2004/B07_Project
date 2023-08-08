720b5604054d53e3b28e932511645cf01bb73e45
Aarya2004 <aarya.prakash.2004@gmail.com>
2023-08-07 21:38:25 -0400

    Changed Store Owner Login to MVP, testing needed

---
+++ app/src/main/java/com/example/b07project/StoreOwnerLoginModel.java
@@ -0,0 +1,46 @@
+package com.example.b07project;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class StoreOwnerLoginModel {
+    FirebaseDatabase db;
+
+    public StoreOwnerLoginModel(){
+        db = FirebaseDatabase.getInstance("https://b07-project-45a16-default-rtdb.firebaseio.com/");
+    }
+
+    public void queryDB(StoreOwnerLoginPresenter presenter, String id){
+        DatabaseReference ref= db.getReference();
+        DatabaseReference query = ref.child("Owners").child(id);
+
+        query.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String password = null;
+                String storeName = null;
+                if(!snapshot.exists()){
+                    presenter.user_does_not_exist();
+                    return;
+                }
+                for(DataSnapshot child: snapshot.getChildren()){
+                    if(String.valueOf(child.getKey()).equals("password")){
+                        password = String.valueOf(child.getValue());
+                    }
+                    if(String.valueOf(child.getKey()).equals("Store Name")){
+                        storeName = String.valueOf(child.getValue());
+                    }
+                }
+                presenter.confirm_user(password, storeName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/b07project/StoreOwnerLoginPresenter.java
@@ -0,0 +1,47 @@
+package com.example.b07project;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+public class StoreOwnerLoginPresenter {
+
+    StoreOwnerLoginModel model;
+    StoreOwnerLoginView view;
+    String userPass;
+
+    public StoreOwnerLoginPresenter(StoreOwnerLoginView view, StoreOwnerLoginModel model) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void check_user_exists(String username, String password, String id) {
+        if (username.equals("") || password.equals(""))
+        {
+            view.displayToast("Username/Password cannot be empty!");
+        }
+        else
+        {
+            userPass = password;
+            model.queryDB(this, id);
+        }
+    }
+
+    public void confirm_user(String password, String storeName){
+        if(!password.equals(userPass)){
+            view.displayToast("Incorrect password!");
+        }else{
+            view.user_confirmed(storeName);
+        }
+    }
+
+    public void user_does_not_exist(){
+        view.displayToast("No such user exists!");
+    }
+}
