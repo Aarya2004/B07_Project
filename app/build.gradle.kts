25129779ef355493500c362ae6eaf38f0d2175a2
Aarya2004 <aarya.prakash.2004@gmail.com>
2023-08-09 02:39:49 -0400

    All Tests added, decent coverage, should be good to submit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.6.0")
     implementation("androidx.navigation:navigation-ui:2.6.0")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-all:1.10.19")
+    testImplementation("org.mockito:mockito-core:5.3.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
