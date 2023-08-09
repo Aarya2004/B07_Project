489fb447934ffbaed98035b9fad2c9eae5450170
Aarya2004 <aarya.prakash.2004@gmail.com>
2023-08-09 14:04:39 -0400

    Removed redundant files from final project

--- app/src/main/java/com/example/b07project/Shopper.java
+++
@@ -1,8 +0,0 @@
-package com.example.b07project;
-
-public class Shopper extends Account{
-
-    public Shopper(String id, String email, String password){
-        super(id, "Shopper", email, password);
-    }
-}
