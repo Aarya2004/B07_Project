651e06ef9d5178390ab23546ada36453ed0bef4c
Syed Rayyan Abbas <syedrayyan.abbas@mail.utoronto.ca>
2023-08-08 23:13:10 -0400

    This contains all of the updated layouts for the store owner. Includes their orders page and products page

---
+++ app/src/main/java/com/example/b07project/Product_Page_StoreOwner.java
@@ -0,0 +1,14 @@
+package com.example.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product_Page_StoreOwner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_page_store_owner);
+    }
+}
