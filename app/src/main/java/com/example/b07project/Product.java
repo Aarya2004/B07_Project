970ce624f59470121b302898dc2ce7d4626e6aac
Syed Rayyan Abbas <syedrayyan.abbas@mail.utoronto.ca>
2023-08-09 11:30:45 -0400

    Updated the code on the Product class so that shoppers or store owners can see the name, brand, price, and quantity for all of the products

--- app/src/main/java/com/example/b07project/Product.java
+++ app/src/main/java/com/example/b07project/Product.java
@@ -1,23 +1,49 @@
 package com.example.b07project;
 
 public class Product {
-    String id;
-    String productName;
-    int price;
+    public String name;
 
-    public Product() {}
+    public String brand;
+    public double price;
+    public int quantity;
 
-    public Product(String id, String name, int price){
-        this.id = id;
-        this.productName = name;
+    public Product(String name, String brand, double price, int quantity) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public String getId(){ return id; }
-    public String getProductName(){ return productName; }
-    public int getPrice(){ return price; }
-    public void setId(String id){ this.id = id; }
-    public void setName(String name){ this.productName = name; }
-    public void setPrice(int price){ this.price = price; }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
+
